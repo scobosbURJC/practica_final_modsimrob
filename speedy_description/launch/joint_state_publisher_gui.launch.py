727d6becc08b5941f806266732aa9cc9ac03939f
scobosb <s.cobosb.2022@alumnos.urjc.es>
2025-05-07 14:17:11 +0200

    joint_state_publisher_gui custom launcher added

--- speedy_description/launch/joint_state_publisher_gui.launch.py
+++ speedy_description/launch/joint_state_publisher_gui.launch.py
@@ -3,20 +3,20 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 import launch_ros.descriptions
 
 def generate_launch_description():
-    prefix = LaunchConfiguration("prefix", default="speedy/")
+    namespace = LaunchConfiguration("namespace", default="speedy/")
 
     joint_state_publisher_gui_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
         output='screen',
-        remappings=[("/robot_description", f"/{prefix}robot_description")],
+        namespace=namespace
     )
 
     nodes = [
